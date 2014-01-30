c2f6b8dfaf3129d098ba04795bc420149944a1ab
noeyfan <youfan.noey@gmail.com>
2014-01-29 23:40:51 -0500

    sass

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 SampleProject::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
+  get "users/new"
+	# get "static_pages/home"
+	# get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+	root 'static_pages#home'
+	match '/', to: 'static_pages#home', via: 'get'
+	match 'signup', to: 'users#new', via: 'get'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
