7f5e706d30c70f20c12baf57bc06b216182ebd81
noeyfan <youfan.noey@gmail.com>
2014-02-06 20:06:35 -0500

    finish add photo

--- Gemfile
+++ Gemfile
@@ -9,6 +9,11 @@ gem 'faker', '1.1.2'
 gem 'will_paginate', '3.0.4'
 gem 'bootstrap-will_paginate', '0.0.9'
 
+#attach photo
+gem 'paperclip', '~> 3.0'
+
+#connect to aws
+
 group :development, :test do
   gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,9 +1,9 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order('created_at DESC') }
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { maximum: 1400 }
   validates :user_id, presence: true
-
+	has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   # Returns microposts from the users being followed by the given user.
   def self.from_users_followed_by(user)
     followed_user_ids = "SELECT followed_id FROM relationships
